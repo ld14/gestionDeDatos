ab5eb125b0bc86892dbc3f53300443c8df8f4cf8
Joaro92 <Joaro_92@yahoo.com.ar>
2016-05-26 11:44:03 -0300

    Delete carga de datos basicos.sql

--- carga de datos basicos.sql
+++
@@ -1,5 +0,0 @@
-select Cli_Mail,Cli_Dom_Calle, Cli_Nro_Calle, Cli_Piso, Cli_Depto, Cli_Cod_Postal
-from gd_esquema.Maestra
-where Cli_Dni is not null
-group by Cli_Mail,Cli_Dom_Calle, Cli_Nro_Calle, Cli_Piso, Cli_Depto, Cli_Cod_Postal, Cli_Dni
-order by Cli_Dni
